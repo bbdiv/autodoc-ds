d632a4282b3ece85e4154c6179b34ed0a824b6a4
bbautodoc <bbatista@autodoc.com.br>
2025-06-11 17:24:47 -0300

    chore: nao sei

--- vite.config.ts
+++ vite.config.ts
@@ -13,19 +13,19 @@ export default defineConfig({
     tsconfigPaths(),
     dts({ insertTypesEntry: true }) // Generate .d.ts files:contentReference[oaicite:0]{index=0}
   ],
-//   build: {
-//     lib: {
-//       entry: path.resolve(__dirname, 'src/index.tsx'),
-//       name: 'AutodocDS',        // UMD global name (if needed)
-//       formats: ['es', 'cjs'],   // ESM and CommonJS outputs
-//       fileName: (format) => `autodoc-ds.${format}.js`
-//     },
-//     rollupOptions: {
-//       // Prevent bundling React into the library (peer dependency)
-//       external: ['react', 'react-dom'],
-//       output: {
-//         globals: { react: 'React', 'react-dom': 'ReactDOM' }
-//       }
-//     }
-//   }
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.tsx'),
+      name: 'AutodocDS',        // UMD global name (if needed)
+      formats: ['es', 'cjs'],   // ESM and CommonJS outputs
+      fileName: (format) => `autodoc-ds.${format}.js`
+    },
+    rollupOptions: {
+      // Prevent bundling React into the library (peer dependency)
+      external: ['react', 'react-dom'],
+      output: {
+        globals: { react: 'React', 'react-dom': 'ReactDOM' }
+      }
+    }
+  }
 });
